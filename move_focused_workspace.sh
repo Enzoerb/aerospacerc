cf3687cd79aa7bf1b9a78a799480d601941c7599
Enzoerb <enzoerbert@gmail.com>
2025-05-23 20:21:51 -0300

    better managing my monitors

--- move_focused_workspace.sh
+++ move_focused_workspace.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # $1 = monitor ID (1, 2, or 3)
-focused_workspace=$(aerospace-msg -t workspace)
+focused_workspace=$(aerospace list-workspaces --focused)
 aerospace move-workspace-to-monitor --workspace "$focused_workspace" "$1"
